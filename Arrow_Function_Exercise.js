11e1fc4cacaa2545e799c7e4628ffa9e37ac59af
Dan Vo <Dan@DVos-MacBook-Pro.local>
2021-12-27 19:02:31 -0800

    Arrow Functions Exercise

---
+++ Arrow_Function_Exercise.js
@@ -0,0 +1,32 @@
+// ES2015 Arrow Functions Shorthand 
+
+// function double(arr) {
+//     return arr.map(function(val) {
+//         return val * 2;
+//     });
+// }
+
+// Refactor the above code to use two arrow functions. Turn it into a one liner.
+
+const double = (arr) => arr.map((val) => val * 2);
+
+
+// Refactor the following function to use arrow functions: 
+// function squareAndFindEvents(numbers){
+//     var squares = numbers.map(function(num){
+//         return num ** 2;
+//     });
+//     var evens = squares.filter(function(square){
+//     return square % 2 === 0;
+//     });
+//     return evens;
+// }
+
+
+const squareAndFindEvents = numbers => numbers
+    .map(num => num ** 2)
+    .filter (square => square % 2 === 0)
+
+
+
+
